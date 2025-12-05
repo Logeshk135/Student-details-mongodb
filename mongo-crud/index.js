6a854d40d86687f8420bcc9cf319a608c9b7e8a1
Logeshk135 <Logeshk135@gmail.com>
2025-12-05 16:12:42 +0530

    updated

--- mongo-crud/index.js
+++ mongo-crud/index.js
@@ -1,18 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const studentRoutes = require("./routes/students");
+import express from "express";
+import connectDB from "./db.js";
+import studentRoutes from "./routes/students.js";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
-app.use("/api/students", studentRoutes);
+// Connect to MongoDB
+connectDB();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+// Routes
+app.use("/students", studentRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
